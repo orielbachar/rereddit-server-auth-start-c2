daae36d110bad0583a66ea977e2c9ee988adfce9
amhayslip <amhayslip@gmail.com>
2016-12-29 08:34:20 +0200

    fix a couple things yo

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(express.static('public'))
+app.use(express.static('node_modules'))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
-app.use('/users', users); 
+app.use('/users', users);
 
 var port = process.env.PORT || '4000';
 
